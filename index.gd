25a5636c7c334127f5be5081adcf2c884fe80343
Viet Nguyen <baviet.nguyen@mail.mcgill.ca>
2020-07-13 16:37:30 -0400

    test

--- index.gd
+++
@@ -1,4 +0,0 @@
-# Viet Nguyen
-![profile photo]()
-I'm an undergrad at [McGill](https://www.mcgill.ca) and research assistant at [Mila](https://mila.quebec) under [Prof. Doina Precup](https://www.cs.mcgill.ca/~dprecup/), 
-where I work in deep exploration for reinforcement learning. 
